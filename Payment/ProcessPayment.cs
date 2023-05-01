0878620b40ad34ce5ffb4f8fe1b92cd12c0eaa00
Michael C <mlclark415@gmail.com>
2023-05-01 12:18:31 -0400

    first commit

---
+++ Payment/ProcessPayment.cs
@@ -0,0 +1,58 @@
+﻿using Stripe;
+using StripeCheckout.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StripeCheckout.Payment
+{
+    public class ProcessPayment
+    {
+        public static async Task<dynamic> PayAsync(PayModel payModel)
+        {
+            try
+            {
+                StripeConfiguration.ApiKey = "your secret key";
+
+                var options = new TokenCreateOptions
+                {
+                    Card = new TokenCardOptions
+                    {
+                        Number = payModel.CardNumder,
+                        ExpMonth = payModel.Month,
+                        ExpYear = payModel.Year,
+                        Cvc = payModel.CVC
+                    },
+                };
+
+                var serviceToken = new TokenService();
+                Token stripeToken = await serviceToken.CreateAsync(options);
+
+                var chargeOptions = new ChargeCreateOptions
+                {
+                    Amount = payModel.Amount,
+                    Currency = "usd",
+                    Description = "Stripe Test Payment",
+                    Source = stripeToken.Id
+                };
+
+                var chargeService = new ChargeService();
+                Charge charge = await chargeService.CreateAsync(chargeOptions);
+
+                if(charge.Paid)
+                {
+                    return "Success";
+                }
+                else
+                {
+                    return "Failed";
+                }
+            }
+            catch(Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+## Stripe-Checkout-ASP.NET-Core-3.1-MVC
+Stripe Payment Gateway integration using ASP.NET Core 3.1 MVC for online payments
+
+## Tools and Languages
+* Microsoft Visual Studio 2019
+* Asp.NET Core 3.1 
+* .NET Core SDK 3.1
+* C#
+
+## Screenshots
+
+![Home Screen](https://github.com/nirab25/Stripe-Checkout-ASP.NET-Core-3.1-MVC/blob/master/wwwroot/images/stripe.JPG)
+
+**Stripe Payment Dashboard**
+
+![Stripe Dashboard.](https://github.com/nirab25/Stripe-Checkout-ASP.NET-Core-3.1-MVC/blob/master/wwwroot/images/stripe2.JPG)
